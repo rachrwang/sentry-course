35ce885fa2159787b453707dda367f10153a1c9d
Rachel Wang <rachelwang@C4C0M957RJ.local>
2023-06-13 18:24:10 -0700

    rachel tries again

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,9 +2,11 @@
 
 const user = {};
 
+</script>
+
 <template>
   <div>
     <h1 class="text-4xl">About</h1>
-    <button> @click="user.email.send()">Send Email</button>
+    <button @click="user.email.send()">Send Email</button>
   </div>
 </template>
